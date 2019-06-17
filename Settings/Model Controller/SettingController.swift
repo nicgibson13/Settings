5249ed6a2dc115aefa67293a121c69b85fb27ac1
Nic Gibson <nic@shpangy.com>
2019-06-17 11:23:05 -0600

    Completed Model Controller

--- Settings/Model Controller/SettingController.swift
+++ Settings/Model Controller/SettingController.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 class SettingController {
-
+    
+    static let sharedInstance = SettingController()
+    
+    let settings: [Setting] = {
+        
+        let music = Setting(name: "iTunes", icon: #imageLiteral(resourceName: "iTunes"), isOn: false)
+        let appStore = Setting(name: "App Store", icon: #imageLiteral(resourceName: "appStore"), isOn: false)
+        let iBooks = Setting(name: "iBooks", icon: #imageLiteral(resourceName: "iBooks"), isOn: false)
+        
+        return[music, appStore, iBooks]
+    }()
+    
+    func toggleIsOn(for setting: Setting) {
+        setting.isOn = !setting.isOn
+    }
 }
